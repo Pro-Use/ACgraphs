bde4260b70b08252e48d08ab554a10d3f5ce2af5
pro-use <robertprouse@gmail.com>
2019-07-04 12:16:45 +0100

    rssTest

--- server/rssTest.py
+++ server/rssTest.py
@@ -3,6 +3,10 @@ NewsFeed = feedparser.parse("http://feeds.bbci.co.uk/news/rss.xml")
 
 print(NewsFeed.entries[1].keys())
 
+for entry in NewsFeed.entries:
+    print("%s|%s|%s" % (entry.link, entry.title, entry.published))
+    print('-' * 20)
+
 
 # if 'title' in NewsFeed.entries[1].keys():
 #     html_content = ""
